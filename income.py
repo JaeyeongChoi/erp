86c7cf7184a52ed9b5c9a4be36b75a66ec3d5811
JaeyeongChoi <go1736@gmail.com>
2020-09-17 15:13:53 +0900

    income test

---
+++ income.py
@@ -0,0 +1,24 @@
+import openpyxl
+
+def income(filename, filename2):
+#    filename = "aa.xlsx"
+#    filename2 = "bb.xlsx"
+    wb = openpyxl.load_workbook(filename)
+    wb2 = openpyxl.load_workbook(filename2)
+    sheet = wb["Sheet1"]
+    sheet2 = wb2["Sheet1"]
+    i=1
+    while(sheet.cell(i,1).value):
+        sheet2.cell(i,1).value = "test"
+        i=i+1
+#use wb.get_highest_row()
+#    sheet2['A1'].value = sheet['A1'].value
+    #sheet.title = "testtest"
+
+    wb.save(filename)
+    wb2.save(filename2)
+
+
+filename = "aa.xlsx"
+filename2 = "bb.xlsx"
+income(filename, filename2)
